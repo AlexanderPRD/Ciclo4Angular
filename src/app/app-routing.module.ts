ad3a19613fbf20e415142036dbb3c420650a89fe
AlexanderPRD <alexander2217@gmail.com>
2021-11-24 21:48:14 -0500

    enrutamientos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'encomiendas',
+    loadChildren: () => import('./modulos/encomienda/encomienda.module').then(m => m.EncomiendaModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/error'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from 'src/app/assets/index/index.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'logaut',
+    component: IndexComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
