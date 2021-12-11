298cd0ef92a5f960b814acb4cf0177772771616c
AlexanderPRD <alexander2217@gmail.com>
2021-12-11 00:37:25 -0500

    info

--- src/app/modelos/servicio.model.ts
+++ src/app/modelos/servicio.model.ts
@@ -1,2 +1,10 @@
-
+export class EncomiendaModelo{
+    id?: String;
+    origen?: String;
+    destino?: string;
+    fecha?: Date;
+    hora?: string;
+    encomienda?: string;
+    valor?: string;
+  }
   
